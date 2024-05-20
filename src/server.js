9de67861dbf4f6cdd8d64b353f6be2b678a35af9
tarcisio <tarcisiomateus98@gmail.com>
2024-05-20 15:49:17 -0300

    created src/middlewares/json.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import http from 'node:http'
+import { json } from './middlewares/json'
 
-const server = http.createServer(() => {
+const server = http.createServer( async (req, res) => {
+  const { method, url } = req
 
+  await json(req, res)
+  
 })
 
 const PORT = 3333
